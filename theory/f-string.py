2300efee774275f03e5d45285be412fb33a1fb3a
Dmitriy <dmitriy250300@mail.ru>
2023-05-02 21:12:57 +0300

    02.05.2023

--- theory/f-string.py
+++ theory/f-string.py
@@ -1,7 +1,9 @@
+# f-строка!!!
 apples = 5
 bananas = 3
 name = 'Василия'
 
-# print('У', name, 'есть', apples, 'яблок и ', bananas, 'бананов')
+print('У', name, 'есть', apples, 'яблок и', bananas, 'бананов')
 
 print(f'У {name} есть {apples} яблок и {bananas} бананов')
+
