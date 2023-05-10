d31a14a511fb08b97a258a6763608a3ededbd089
Dmitriy <dmitriy250300@mail.ru>
2023-05-10 11:44:02 +0300

    10.05.2023

---
+++ Archive/10-00_PS2/m5_games/shooter_game/test.py
@@ -0,0 +1,3 @@
+from time import *
+
+print(round(time()))
---
+++ theory/while_cycle.py
@@ -0,0 +1,7 @@
+password = '123'
+answer = input('Пароль!')
+while answer != password:
+    print('Доступ запрещён!')
+    answer = input('Пароль!')
+
+print('Доступ получен')
