eb992c6a9310b912b51783a027692ebec18a73a4
Dmitriy <dmitriy250300@mail.ru>
2023-05-22 09:35:05 +0300

    22.05.2023

--- theory/lists_max_min.py
+++ theory/lists_max_min.py
@@ -6,4 +6,5 @@ print(heapq.nlargest(3, scores))  # [91, 87, 82]
 print(heapq.nsmallest(5, scores))  # [15, 33, 33, 49, 51]
 
 _, *elements_in_the_middle, _ = [1, 2, 3, 4, 5, 6, 7, 8]
-print(elements_in_the_middle)  # [2, 3, 4, 5, 6, 7
+print(elements_in_the_middle)
+# [2, 3, 4, 5, 6, 7
