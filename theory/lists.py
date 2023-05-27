dcb9b19176261ce5e8bf92e395691acb64e58f89
Dmitriy <dmitriy250300@mail.ru>
2023-05-27 13:04:23 +0300

    27.05.2023

--- theory/lists.py
+++ theory/lists.py
@@ -1,7 +1,9 @@
 list1 = [1, 2, 3, 4, 5, 'string', [10, 14, 16]]
 list2 = [22, 23, 24]
 
+
 list1.append(12)
+print(list1)
 list1.insert(2, 9)
 # нежелательно складывать списки с помощью + :
 print(list1+list2)
@@ -13,7 +15,7 @@ list1 = [5, 7, 10]
 # удаление элемента по его индексу
 digit = list1.pop(2)
 # удаление элемента по его значению! удаляется первое значение
-list1.remove(2)
+list1.remove(5)
 
 list1 = [2, 5, 7, 2, 10, 15, 2]
 while 2 in list1:
@@ -22,3 +24,4 @@ while 2 in list1:
 print(list1)
 
 # list1.clear() - полная очистка списка
+
