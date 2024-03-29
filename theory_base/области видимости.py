06cb4e415a4708a589d24c58c80ebb2b82a49e48
Dmitriy <dmitriy250300@mail.ru>
2024-03-29 15:08:25 +0300

    29.03.2023

--- theory_base/области видимости.py
+++ theory_base/области видимости.py
@@ -1,18 +1,21 @@
 def f3():
     '''область всех закрывающих функций '''
+
     def f4():
         '''локальная'''
         global number
         number = 10
         print(number)
+
     global number
     number = 30
     f4()
     print(number)
+
+
 number = 100
 f3()
 print(number)
-
 
 '''
 1) Локальная - local scope. Самая внутренняя область (в текущей функции)
